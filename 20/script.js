7c5b8c481b2efe3490688462161909719519f20c
Gabriel Malinski <78242219+malinskiDEV@users.noreply.github.com>
2024-05-02 21:56:19 -0300

    Add files via upload
    arrays

---
+++ 20/script.js
@@ -0,0 +1,14 @@
+// function nomeParaFuncao (parametro(s)) {
+//     instrucao
+// }
+// ex de funcoes{
+    // alert
+// prompt
+
+function soma(x, y) {
+    let soma = x + y
+    return soma
+}
+
+let resposta = soma(2, 5)
+alert(resposta)
